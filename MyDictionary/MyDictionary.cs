34f7a64362db2d2d62ed41414f33430b6e5650e5
tunahanozcan <77634378+tunahanozcan@users.noreply.github.com>
2021-01-27 10:52:36 +0300

    Add files via upload

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class MyDictionary<Tkey,Tvalue>
+    {
+        Tkey[] tkeys;
+        Tvalue[] tvalues;
+
+        public MyDictionary()
+        {
+            tkeys = new Tkey[0];
+            tvalues = new Tvalue[0];
+        }
+
+        public void Add(Tkey anahtar,Tvalue deger)
+        {
+            Tkey[] tempArray = tkeys;
+            Tvalue[] tempArray2 = tvalues;
+
+            tkeys = new Tkey[tkeys.Length + 1];
+            tvalues = new Tvalue[tvalues.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                tkeys[i] = tempArray[i];
+            }
+
+            for (int i = 0; i < tempArray2.Length; i++)
+            {
+                tvalues[i] = tempArray2[i];
+            }
+
+            tkeys[tkeys.Length - 1] = anahtar;
+            tvalues[tvalues.Length - 1] = deger;
+        }
+
+        public Tkey[] No
+        {
+            get { return tkeys; }
+        }
+        public Tvalue[] Isım
+        {
+            get { return tvalues; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<int, string> ogrenci = new MyDictionary.MyDictionary<int, string>();
+            ogrenci.Add(17, "Tunahan");
+            ogrenci.Add(34, "İlyas");
+            ogrenci.Add(78, "Özcan");
+
+
+            Console.WriteLine($" Öğrencinin Numarası: {ogrenci.No[0]} \n Öğrencinin İsmi: {ogrenci.Isım[0]} \n ******************************** ");
+            Console.WriteLine($" Öğrencinin Numarası: {ogrenci.No[1]} \n Öğrencinin İsmi: {ogrenci.Isım[1]} \n ********************************");
+            Console.WriteLine($" Öğrencinin Numarası: {ogrenci.No[2]} \n Öğrencinin İsmi: {ogrenci.Isım[2]} \n ******************************** ");
+
+            Console.ReadLine();
+        }
+    }
+}
